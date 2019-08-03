b88ee3062dc6046e2d79c351d379ee97ee18e640
leixindi <842706128@qq.com>
2019-08-03 10:24:33 +0800

    修改

--- src/com/leixindi/bean/Person.java
+++ src/com/leixindi/bean/Person.java
@@ -1,20 +1,29 @@
 package com.leixindi.bean;
 
 public class Person {
+	/**
+	 * 用户名
+	 */
 	private String userName;
+	/**
+	 * 年龄
+	 */
 	private int age;
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
+
 }
